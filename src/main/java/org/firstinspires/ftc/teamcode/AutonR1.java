4e6df66001118e7ade494ffc377fbc83036f110f
Wei Wang <weiwang@umich.edu>
2019-12-08 09:24:56 -0500

    Autonomous modes: shells for Blue/Red alliance positions 1 & 2

---
+++ src/main/java/org/firstinspires/ftc/teamcode/AutonR1.java
@@ -0,0 +1,60 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/** AutonModeR1: Red 1 Position
+ *
+ * This Autonomous opmode assumes that the robot starts in the Red position 1, on the side of the
+ * Foundation, facing towards the Foundation. The tasks are:
+ *
+ * 1.   Move Foundation to Building Site;
+ * 1.1. Drive to Foundation;
+ * 1.2. Lower a hand to the Foundation;
+ * 1.3. Drive to Building Site;
+ * 1.4.
+ * 2.   Transport Skystone
+ * 3.   Parking
+ * 3.1. Determine current position and orientation;
+ * 3.2. Driver towards Skybridge - detect color while driving;
+ * 3.3. Stop when color is detected.
+ *
+ * Reference: https://drive.google.com/open?id=1HdyA5MHN3-CSbFCGKsrEqOEmcXNH-F_7
+ * Reference: org.firstinspires.ftc.robotcontroller.external.samples.PushbotAutoDrive*
+ */
+
+@Autonomous(name="AutonFSM: Red foundation 0", group="Auton")
+public class AutonR1 extends AutonFSM1
+{
+    public AutonR1() {
+        super(true);
+    }
+}
