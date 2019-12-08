0ab7ca9a14584aa18ee5ead9e1e5c36a4c163995
Wei Wang <weiwang@umich.edu>
2019-12-08 15:47:49 -0500

    Unit test for foundation pulling

---
+++ src/main/java/org/firstinspires/ftc/teamcode/TestPullFoundation.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Test: Pull foundation", group="Test")
+public class TestPullFoundation extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RobotConfig robot = RobotConfig.init(this, MecanumDrive.DriveMode.MANUAL);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        telemetry.addData("Status", "Press Start to test foundation pull");
+        telemetry.update();
+        robot.start();
+
+        // Send values to the motors
+        robot.puller_up();
+        robot.showtime();
+        robot.drive_over(16.1);
+        robot.puller_down();
+        robot.drive_over(-16.2);
+        robot.puller_up();
+        robot.showtime();
+        robot.stop();
+    }
+}
