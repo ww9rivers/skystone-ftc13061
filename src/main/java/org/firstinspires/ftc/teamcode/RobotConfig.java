10cca7f4028d1846241a2c7b7f4e1edc73431d96
Wei Wang <weiwang@umich.edu>
2020-01-05 21:49:59 -0500

    Fixed earlier conflicts that somehow got committed

--- src/main/java/org/firstinspires/ftc/teamcode/RobotConfig.java
+++ src/main/java/org/firstinspires/ftc/teamcode/RobotConfig.java
@@ -83,52 +83,29 @@ public class RobotConfig extends MecanumDrive
     public DigitalChannel   touchSensor     = null;
     public ElapsedTime      runtime         = new ElapsedTime();
 
-<<<<<<< HEAD
     // NOTE: Different copy-and-pasted code use different names for these constants.
     //       Use both sets, but still trying to keep things understandable.
     // https://www.reddit.com/r/FTC/comments/7s6y32/rev_hd_hex_motor_encoder_cpr/
     private static final int ENCODER_CPR_HEXHD  = 1120;     // CPR of REV Hex HD Motor (REV-41-1301)
     private static final int ENCODER_CPR_TETRIX = 1440;     // CPR of Tetrix DC motor
+    private static final double DRIVE_GEAR_REDUCTION    = 1.175 ;     // This is < 1.0 if geared UP
+    private static final double WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    // CPI calculation should be: COUNTS_PER_INCH =
+    //  (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * Math.PI)
     private static final int ENCODER_CPR        = ENCODER_CPR_HEXHD;    // We are using REV Hex HD motors
-    private static final double PULLER_DOWN     = 0.0;      // Maximum rotational position
-=======
-    // https://www.reddit.com/r/FTC/commenats/7s6y32/rev_hd_hex_motor_encoder_cpr/
-    private static final int ENCODER_CPR        = 1120;     // REV Hex HD Motor (REV-41-1301) CPR is 1120
-    private static final double ENCODER_CPT     = 1440;     // number of encoder clicks in a full turn
     private static final double ENCODER_CPI     = 53.476;   // encoder clicks per inch
     private static final double PULLER_DOWN     = 1.0;      // Maximum rotational position
->>>>>>> fsm/master
     private static final double PULLER_UP       = 0.5;      // Maximum rotational position
     public static final double MID_SERVO        = 0.5 ;
     public static final double ARM_UP_POWER     = 0.45 ;
     public static final double ARM_DOWN_POWER   = -0.45 ;
-<<<<<<< HEAD
-    static final double COUNTS_PER_MOTOR_REV    = ENCODER_CPR;  // REV Hex HD motor w/ encoder
-    static final double DRIVE_GEAR_REDUCTION    = 1.175 ;       // This is < 1.0 if geared UP
-    static final double WHEEL_DIAMETER_INCHES   = 4.0 ;         // For figuring circumference
-    static final double COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * Math.PI);
-    private static final double ENCODER_CPI     = COUNTS_PER_INCH;  // encoder counts per inch
-    public static final double DRIVE_SPEED      = 0.6;
+    public static final double DRIVE_SPEED      = 0.7;
     public static final double TURN_SPEED       = 0.5;
-
-    public static final String STATUS           = "Status";
-    public static final String TOUCH_SENSOR     = "touch_sensor";
-=======
-
-    private static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
-    private static final double     DRIVE_GEAR_REDUCTION    = 1.175 ;     // This is < 1.0 if geared UP
-    private static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
-    private static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
-            (WHEEL_DIAMETER_INCHES * Math.PI);
-    public static final double     DRIVE_SPEED             = 0.7;
-    public static final double     TURN_SPEED              = 0.5;
 
     public static final String STATUS           = "Status";
     public static final String TOUCH_SENSOR     = "touch_sensor";
     public static final String COLOR_SENSOR     = "color_sensor";
     public static final int NUMBER_OF_SERVOS    = 5;
->>>>>>> fsm/master
     private static final String SERVOS[]        = {
         "servo0", "servo1", "servo2", "servo3", "servo4"
     };
@@ -254,7 +231,6 @@ public class RobotConfig extends MecanumDrive
         return init(opmode, DriveMode.MANUAL);
     }
 
-<<<<<<< HEAD
     /**
      * Stop and reset drive motor encoders.
      */
@@ -267,7 +243,8 @@ public class RobotConfig extends MecanumDrive
      */
     public boolean detect_touch () {
         return touchSensor.getState();
-=======
+    }
+
     private void resetDriveMode(DcMotor motor) {
         // Set the drive motor run modes:
         // "RUN_USING_ENCODER" causes the motor to try to run at the specified fraction of full velocity
@@ -279,7 +256,6 @@ public class RobotConfig extends MecanumDrive
         }
         else
             motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
->>>>>>> fsm/master
     }
 
     /**
@@ -621,7 +597,6 @@ public class RobotConfig extends MecanumDrive
     /**
      * Stop the robot.
      */
-
     public void stop () {
         theRobot = null;
         drive(0,0,0,0);
@@ -630,7 +605,6 @@ public class RobotConfig extends MecanumDrive
         app.telemetry.update();
     }
 
-<<<<<<< HEAD
     /**
      * Turn the robot a given angle: Positive to turn counter-clock wise.
      *
@@ -657,8 +631,6 @@ public class RobotConfig extends MecanumDrive
         turn_left(-power);
     }
 
-=======
->>>>>>> fsm/master
     public final void sleep(long milliseconds) {
         try {
             Thread.sleep(milliseconds);
@@ -669,11 +641,6 @@ public class RobotConfig extends MecanumDrive
 
     private void robotReset () {
         // Stop all motion;
-<<<<<<< HEAD
-        pause();
-        reset_motor_encoder();
-        drive_using_encoder();
-=======
         leftFrontMotor.setPower(0);
         rightFrontMotor.setPower(0);
         leftRearMotor.setPower(0);
@@ -705,9 +672,5 @@ public class RobotConfig extends MecanumDrive
      */
     public boolean detect_touch () {
         return touchSensor.getState();
->>>>>>> fsm/master
     }
-
-
-
 }
